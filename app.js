6c054e6dca01648b16e130ed63363ac52e7dc2f3
Olatunji Peter <63746720+Peteru2@users.noreply.github.com>
2025-09-30 19:23:08 +0100

    app file modified

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 const express = require('express');
-const http = require('http');
 const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./db.js');
 const Controller = require('./controller/verifyController.js');
 const userController = require('./controller/userController.js');
 const transHistoryController = require('./controller/transHistoryController.js');
-
-
 const authMiddleware = require('./auth.js');
 
 const app = express();
+
 const allowedOrigins = [
-  "http://localhost:5173",        
-  // "https://your-frontend.vercel.app" 
+  "http://localhost:5173",
+  "https://your-frontend.vercel.app" 
 ];
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-      return callback(new Error(msg), false);
+      return callback(new Error('CORS blocked this origin.'), false);
     }
     return callback(null, true);
   },
@@ -33,23 +29,27 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-connectDB()
-// .then(() => {
-//   server.listen(process.env.PORT, "0.0.0.0", () => {
-//     console.log('Server is running on port 8000');
-//   });
-// });
+
+connectDB();
+
 
 app.post('/SignUp', Controller.Post_signUp);
 app.post('/Login', Controller.Post_login);
 app.post('/verifyEmail', Controller.verifyEmail);
 
-app.all('*', authMiddleware)
+app.use(authMiddleware);
 
-app.get('/', authMiddleware, userController.Get_user);
-app.put('/updateTransactionPin', authMiddleware, Controller.UpdateTransPin);
-app.put('/updatekyc', authMiddleware, Controller.UpdateKyc);
-app.get('/balance', authMiddleware, Controller.GetBalance);
-app.post('/val_transfer', authMiddleware, Controller.Check_transfer);
-app.post('/transfer', authMiddleware, Controller.Post_transfer);
-app.get('/trans-history', authMiddleware, transHistoryController.Transfer_history);
+app.get('/', (req, res) => {
+  res.json({ message: "API running on Vercel 🚀" });
+});
+
+app.get('/user', userController.Get_user);
+app.put('/updateTransactionPin', Controller.UpdateTransPin);
+app.put('/updatekyc', Controller.UpdateKyc);
+app.get('/balance', Controller.GetBalance);
+app.post('/val_transfer', Controller.Check_transfer);
+app.post('/transfer', Controller.Post_transfer);
+app.get('/trans-history', transHistoryController.Transfer_history);
+
+
+module.exports = app;
