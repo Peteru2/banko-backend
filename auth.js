874d74962bbd25ec9a88f49d96e9cb31c1155eb2
Olatunji Peter <63746720+Peteru2@users.noreply.github.com>
2025-11-05 16:57:43 +0100

    still battling with vercel deployment

--- auth.js
+++ auth.js
@@ -1,26 +1,25 @@
 const jwt = require("jsonwebtoken");
-const { User } = require("./models/User.js");
-
+const {User} = require("./models/User.js")
 const authMiddleware = async (req, res, next) => {
-  const token =
-    req.headers.authorization &&
-    req.headers.authorization.split(" ")[1];
+  const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ error: 'Authorization token is missing' });
+  }
 
-  // Allow public requests if no token
-  if (!token) return next();
-
-  try {
+try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    // req.user = decoded;
+  const user = await User.findById(decoded.userId).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
 
-    const user = await User.findById(decoded.userId).select("-password");
-    if (!user) return res.status(404).json({ message: "User not found, create an account" });
-
-    req.user = { ...user._doc, userId: user._id };
-
+    req.user = {
+  ...user._doc,
+  userId: user._id,
+};
     next();
   } catch (error) {
-    return res.status(401).json({ error: "Invalid or expired token " });
+    return res.status(401).json({ error: 'Invalid or expired token' });
   }
 };
 
-module.exports = authMiddleware;
+module.exports = authMiddleware
--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-// require("dotenv").config();
-// const app = require("./app");
-
-// const PORT = process.env.PORT || 8000;
-
-// app.listen(PORT, () => {
-//   console.log(`Server running locally at http://localhost:${PORT}`);
-// });
-
-
 require("dotenv").config();
 const app = require("./app");
-module.exports = app; 
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running locally at http://localhost:${PORT}`);
+});
+
+
+
