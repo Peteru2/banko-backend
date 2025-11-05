28b76abbb388d9855693ff295acbd8e6d5fcd471
Olatunji Peter <63746720+Peteru2@users.noreply.github.com>
2025-11-05 16:20:44 +0100

    vercel deployment still ongoing

--- auth.js
+++ auth.js
@@ -13,13 +13,13 @@ const authMiddleware = async (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
     const user = await User.findById(decoded.userId).select("-password");
-    if (!user) return res.status(404).json({ message: "User not found" });
+    if (!user) return res.status(404).json({ message: "User not found, create an account" });
 
     req.user = { ...user._doc, userId: user._id };
 
     next();
   } catch (error) {
-    return res.status(401).json({ error: "Invalid or expired token" });
+    return res.status(401).json({ error: "Invalid or expired token " });
   }
 };
 
